12685914907f0f918096bc02f8cef0114735b9f6
Rusl16 <greshyshchev.r@gmail.com>
2016-09-05 17:48:45 +0300

    first commit

--- home/about_us.php
+++ home/about_us.php
@@ -8,7 +8,43 @@
  */
 get_header();?>
 <script src="/wp-content/themes/garnet-theme/js/onlyHome.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8jDXBP52zsQclHleKItMuZMjJhZGxGOk&callback=initMap"
+        async defer></script>
+<script>
+    function initMap() {
 
+        // Specify features and elements to define styles.
+        var odessaStyle = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}];
+        var kievStyle = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#ff0000"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"lightness":"100"}]},{"featureType":"landscape.man_made","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"lightness":"100"}]},{"featureType":"landscape.natural","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural.landcover","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"lightness":"100"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry.fill","stylers":[{"visibility":"off"},{"lightness":"23"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffd900"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ffd900"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#cccccc"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]}];
+
+        odessa = new google.maps.Map(document.getElementById('map__l'), {
+            center: {lat: 46.471610, lng: 30.736011},
+            zoom: 12,
+            styles: odessaStyle,
+            scrollwheel: false
+        });
+
+        kiev = new google.maps.Map(document.getElementById('map__r'), {
+            center: {lat: 50.3944334, lng: 30.4775495},
+            zoom: 12,
+            styles: kievStyle,
+            scrollwheel: false
+        });
+
+        var markerOdessa = new google.maps.Marker({
+            position: {lat: 46.471610, lng: 30.736011},
+            map: odessa,
+            icon: "/lp/images/marker.png"
+        });
+
+        var markerKiev = new google.maps.Marker({
+            position: {lat: 50.3944334, lng: 30.4775495},
+            map: kiev,
+            icon: "/lp/images/marker.png"
+        });
+    }
+
+</script>
     <section>
         <div class="about">
             <div class="container">
@@ -305,6 +341,29 @@ get_header();?>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="map__serv">
+            <div class="col-md-6">
+                <div class="row --m">
+                    <div id="map__l"></div>
+                    <div class="text__l">
+                        <p><span>г. Одесса,</span> Малая Арнаутская 64</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row --m">
+                    <div id="map__r"></div>
+                    <div class="text__r">
+                        <p><span>г. Киев,</span> Ахтырский переулок 3</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cont__cntr">
+                
             </div>
         </div>
     </section>
