2473a03a41d29028e1278a8fee7a5e4a982522bf
Rusl16 <greshyshchev.r@gmail.com>
2016-09-05 16:16:48 +0300

    our serv

--- home/about_us.php
+++ home/about_us.php
@@ -4,13 +4,310 @@
  * User: Руслан
  * Date: 05.09.2016
  * Time: 10:30
- * Template Name : О Нас
+ * Template Name: О Нас
  */
 get_header();?>
+<script src="/wp-content/themes/garnet-theme/js/onlyHome.js"></script>
 
-
-
-
+    <section>
+        <div class="about">
+            <div class="container">
+                <div class="title">
+                    <h1><span>Garnet</span> software &mdash; веб-студия</h1>
+                </div>
+                <div class="main__ideas">
+                    <p>идеи</p>
+                    <span>&</span>
+                    <p>решения</p>
+                    <img src="/wp-content/themes/garnet-theme/img/brain.png" alt="and">
+                    <a href="#ab__1" class="arrow__down"><img src="/wp-content/themes/garnet-theme/img/arrow.png" alt="arrow"></a>
+                </div>
+                <div id="ab__1" class="ab__main">
+                    <div class="col-md-4">
+                        <div class="desc">
+                            <p><span>Мы</span> &mdash; эксперты</p>
+                            <p>в современных</p>
+                            <p>технологиях</p>
+                            <p>создания <span>сайтов</span></p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="cms">
+                            <p><span>У нас</span></p>
+                            <p>собственная</p>
+                            <p><span>CMS система</span></p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="offer">
+                            <p><span>Мы </span> предоставляем</p>
+                            <p>качественную</p>
+                            <p><span>тех</span>поддержку</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="text text-center">
+                    <p>
+                        Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил, что он у себя в постели превратился
+                        в страшное насекомое. Лежа на панцирнотвердой спине, он видел, стоило ему приподнять голову, свой
+                        коричневый, выпуклый, разделенный дугообразными чешуйками живот, на верхушке которого еле
+                        держалось готовое вот-вот окончательно сползти одеяло.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+    <section>
+        <div class="team">
+            <div class="container">
+                <div class="title text-center">
+                    <h2>Кто работает над Вашим проектом</h2>
+                </div>
+                <div class="who__works row">
+                    <div class="col-md-4 l__side">
+                        <div class="marketers photo__t">
+                            <span></span>
+                            <span></span>
+                            <h4>marketers</h4>
+                            <p>Душа озарена неземной
+                                радостью, как эти чудесные
+                                весенние утра, которыми я
+                                наслаждаюсь</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 l__side">
+                        <div class="ceo photo__t">
+                            <span></span>
+                            <h4>ceo</h4>
+                            <p>Душа озарена неземной
+                                радостью, как эти чудесные
+                                весенние утра, которыми я
+                                наслаждаюсь</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 l__side">
+                        <div class="account photo__t">
+                            <span></span><span></span>
+                            <h4>Account managers</h4>
+                            <p>Душа озарена неземной
+                                радостью, как эти чудесные
+                                весенние утра, которыми я
+                                наслаждаюсь</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 l__side">
+                        <div class="project photo__t">
+                            <div class="owl__c">
+                                <span class="pr__1"></span>
+                                <span class="pr__2"></span>
+                                <span class="pr__3"></span>
+                            </div>  
+                            <h4>Project managers</h4>
+                            <p>Душа озарена неземной
+                                радостью, как эти чудесные
+                                весенние утра, которыми я
+                                наслаждаюсь</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="request">
+                            <img src="/wp-content/themes/garnet-theme/img/1-01.svg" alt="work-team_logo">
+                            <a href="#" class="modal__btn"><span>Подать заявку</span></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 l__side">
+                        <div class="designers photo__t">
+                            <span></span>
+                            <span></span>
+                            <h4>designers</h4>
+                            <p>Душа озарена неземной
+                                радостью, как эти чудесные
+                                весенние утра, которыми я
+                                наслаждаюсь</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 --s__1">
+                        <div class="devel photo__t">
+                            <h4>web developers</h4>
+                            <p>Душа озарена неземной
+                                радостью, как эти чудесные
+                                весенние утра, которыми я
+                                наслаждаюсь</p>
+                        </div>
+                    </div>
+                    <div class="col-md-8 --s__1">
+                        <div class="row">
+                            <div class="developers">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="free__l text-center">
+                        <p><span>+</span> <span>50</span> удалённых разработчиков</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<section>
+    <div class="main__c">
+        <div class="container">
+            <div class="row">
+                <div class="main__title wow rotateInDownLeft" data-wow-delay="0.4s">
+                    <h3>Нам есть чем гордиться</h3>
+                </div>
+                <div class="c__p col-md-3 col-sm-6 col-xs-12"
+                     data-value="0.25"
+                     data-size="204"
+                     data-thickness="6"
+                     data-animation-start-value="0">
+                    <div class="c__a --n"></div>
+                    <p>Мобильных приложений</p>
+                </div>
+                <div class="c__p col-md-3 col-sm-6 col-xs-12"
+                     data-value="0.5"
+                     data-size="204"
+                     data-thickness="6"
+                     data-animation-start-value="0">
+                    <div class="c__b --n"></div>
+                    <p>Клиентов</p>
+                </div>
+                <div class="c__p col-md-3 col-sm-6 col-xs-12"
+                     data-value="0.9"
+                     data-size="204"
+                     data-thickness="6"
+                     data-animation-start-value="0">
+                    <div class="c__c --n"></div>
+                    <p>Завершенных проектов</p>
+                </div>
+                <div class="c__p col-md-3 col-sm-6 col-xs-12"
+                     data-value="0.13"
+                     data-size="204"
+                     data-thickness="6"
+                     data-animation-start-value="0">
+                    <div class="c__d --n"></div>
+                    <p>Лет опыта</p>
+                </div>
+                <div class="desc wow slideInLeft" data-wow-delay="0.4s" data-wow-duration="0.6s">
+                    <p><span>Мы</span> &mdash; эксперты</p>
+                    <p>в современных</p>
+                    <p>технологиях</p>
+                    <p>создания <span>сайтов</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="bgc__2">
+    <section>
+        <div class="main__d">
+            <div class="container">
+                <div class="row --rlc">
+                    <div class="col-md-6 col-sm-12 col-xs-12 --rolling wow bounceInDown" data-wow-delay="0.6s">
+                        <canvas id="c" width="600" height="600"></canvas>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-xs-12 wow bounceInUp" data-wow-delay="0.8s">
+                        <div class="days">
+                            <h2>Наши <span>будни</span></h2>
+                            <?php
+                            if ( have_posts() ) : // если имеются записи в блоге.
+                                query_posts('cat=7');   // указываем ID рубрик, которые необходимо вывести.
+                                while (have_posts()) : the_post();  // запускаем цикл обхода материалов блога
+                                    ?>
+                                    <div class="our__days">
+                                        <h4><?php the_title(); ?></h4>
+                                        <?php the_content();?>
+                                    </div>
+                                <?php endwhile;  // завершаем цикл.
+                            endif;
+                            /* Сбрасываем настройки цикла. Если ниже по коду будет идти еще один цикл, чтобы не было сбоя. */
+                            wp_reset_query();
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="main__l --serv__mod">
+            <div class="container">
+                <div class="row">
+                    <div class="title">
+                        <h2>ЛУчшая награда <span>за наш труд</span></h2>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/company1.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/company4.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/company9.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/company10.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/company11.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/company12.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/dostavka24-01.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/kriger-01.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/kvartal.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3 col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/Lam_Consalt-01.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3  col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/lavar-01.svg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-sm-3  col-xs-4">
+                        <div class="svg__l">
+                            <img class="icon__c img-responsive" src="/wp-content/themes/garnet-theme/img/mad_doner-01.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer();?>
 
