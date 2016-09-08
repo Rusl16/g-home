4028e59abebc094adead4d40a4a169546c8ea740
Rusl16 <greshyshchev.r@gmail.com>
2016-09-08 17:11:09 +0300

    chancge for mobile

--- wp-content/themes/garnet-theme/js/main.js
+++ wp-content/themes/garnet-theme/js/main.js
@@ -37,6 +37,22 @@ $(document).ready(function () {
         autoplay:true,
         responsive:{
             0:{
+                items:1
+            },
+
+            500:{
+                items:2
+            },
+
+            680:{
+                items:3
+            },
+
+            768:{
+                items:4
+            },
+
+            991:{
                 items:6
             },
             1191:{
