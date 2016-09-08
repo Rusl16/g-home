b331fae214962ade6f2710c62e32a5f35005cc94
Rusl16 <greshyshchev.r@gmail.com>
2016-09-08 11:00:03 +0300

    change blog

--- wp-content/themes/garnet-theme/sidebar.php
+++ wp-content/themes/garnet-theme/sidebar.php
@@ -6,39 +6,46 @@
  */
 ?>
 <?php if (is_active_sidebar( 'sidebar' )) { // если в сайдбаре есть что выводить ?>
-<aside class="col-sm-4">
+<aside class="col-md-4">
 
 	<?php dynamic_sidebar('sidebar'); // выводим сайдбар, имя определено в functions.php ?>
-
-	<ul class="nav nav-pills">
-		<li class="active"><a data-toggle="pill" href="#aside-top">Топ</a></li>
-		<li><a data-toggle="pill" href="#aside-recent">Недавнее</a></li>
-
-	</ul>
+	<div class="col-md-12">
+		<ul class="nav nav-pills">
+			<li class="active"><a data-toggle="pill" href="#aside-top">Топ</a></li>
+			<li><a data-toggle="pill" href="#aside-recent">Недавнее</a></li>
+		</ul>
+	</div>
 	<div class="tab-content">
-	<div id="aside-top" class="tab-pane fade in active">
-		<ul class="aside-top-recent">
-			<?php query_posts('category_name=Блог&showposts=3'); ?>
-			<?php while (have_posts()) : the_post(); ?>
-				<li><a class="title-recent" href="<?php the_permalink(); ?>">
-						<?php the_post_thumbnail(large);?>
-						<p><?php the_title(); ?> </p>
-					</a> </li>
-			<?php endwhile; ?>
-		</ul>
-	</div>
-	<div id="aside-recent" class="tab-pane fade">
-		<ul class="aside-top-recent">
-			<?php query_posts('category_name=Блог&showposts=3'); ?>
-			<?php while (have_posts()) : the_post(); ?>
-				<li><a class="title-recent" href="<?php the_permalink(); ?>">
-						<?php the_post_thumbnail(large);?>
-						<p><?php the_title(); ?> </p>
-					</a> </li>
-			<?php endwhile; ?>
-		</ul>
-	</div>
+		<div id="aside-top" class="tab-pane fade in active">
+			<div class="col-md-12">
+				<ul class="aside-top-recent">
+					<?php query_posts('category_name=Блог&showposts=3'); ?>
+					<?php while (have_posts()) : the_post(); ?>
+						<li><a class="title-recent" href="<?php the_permalink(); ?>">
+								<?php the_post_thumbnail(large);?>
+								<p><?php the_title(); ?> </p>
+							</a>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+			</div>
 		</div>
+		<div id="aside-recent" class="tab-pane fade">
+			<div class="col-md-12">
+				<ul class="aside-top-recent">
+					<?php query_posts('category_name=Блог&showposts=3'); ?>
+					<?php while (have_posts()) : the_post(); ?>
+						<li>
+							<a class="title-recent" href="<?php the_permalink(); ?>">
+								<?php the_post_thumbnail(large);?>
+								<p><?php the_title(); ?> </p>
+							</a>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+			</div>
+		</div>
+	</div>
 
 	<ul>
 		<?php kama_recent_comments("limit=3&ex=100"); ?>
